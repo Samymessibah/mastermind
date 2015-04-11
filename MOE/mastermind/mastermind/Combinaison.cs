2a6581fb64fb600ec447a23e42cee99dcd04d2f4
mebatista <m.batista@outlook.fr>
2015-04-11 15:53:33 +0200

    Ajout des classes de combinaisons, symboles et de joueurs.

---
+++ MOE/mastermind/mastermind/Combinaison.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mastermind
+{
+    class Combinaison
+    {
+        List<int> combinaison = new List<int>();
+    }
+}
---
+++ MOE/mastermind/mastermind/codeur.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mastermind
+{
+    class Codeur : IJoueur
+    {
+
+        private String pseudo {
+            get
+            {
+                return pseudo;
+            }
+
+            set
+            {
+                pseudo = value;
+            }
+        }
+
+        // private Conteneur
+
+
+    }
+}
