04493121ec2245a6023929695399592f6d98b21f
jordi portell <portelljordi@gmail.com>
2015-04-12 23:39:22 +0200

    Test Unitaire
    Création du test unitaire de la méthode formater.

---
+++ MOE/mastermind/mastermind/TestUnitaire.cs
@@ -0,0 +1,25 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace mastermind
+{
+    [TestClass]
+ public   class TestUnitaire
+    {
+            [TestMethod]
+            public void TestFormater()
+            { 
+                int [] tab ={1,2,3,5};
+                Formatter r = new Formatter();
+              //  Console.WriteLine(r.formater(tab));
+                //Console.Read();
+                Assert.AreEqual(" 1 2 3 5", r.formater(tab));
+            
+        }
+    }
+}
